c5008f280bffaf8c5684635895ae59256c6728f7
Irfan Alibay <IAlibay@users.noreply.github.com>
2020-07-27 00:21:09 +0100

    Adds more tests (#28)
    * Increases test coverage
    
    * adds hostguest regression tests

--- MDRestraintsGenerator/tests/datafiles.py
+++ MDRestraintsGenerator/tests/datafiles.py
@@ -5,6 +5,8 @@ Location of datafiles for tests
 __all__ = [
         "T4_TPR", "T4_XTC",
         "T4_OGRO", "T4_OTOP",
+        "CB8_TPR", "CB8_XTC",
+        "CB8_OGRO", "CB8_OTOP",
 ]
 
 from pkg_resources import resource_filename
@@ -16,3 +18,11 @@ T4_OGRO = resource_filename(__name__,
                             '../data/4RBN/ClosestRestraintFrame.gro')
 T4_OTOP = resource_filename(__name__,
                             '../data/4RBN/BoreschRestraint.top')
+CB8_TPR = resource_filename(__name__,
+                            '../data/CB8-G3/npt_prod1.tpr')
+CB8_XTC = resource_filename(__name__,
+                            '../data/CB8-G3/npt_prod_center.skip200.xtc')
+CB8_OGRO = resource_filename(__name__,
+                             '../data/CB8-G3/ClosestRestraintFrame.gro')
+CB8_OTOP = resource_filename(__name__,
+                             '../data/CB8-G3/BoreschRestraint.top')
--- MDRestraintsGenerator/tests/test_FindBoreschRestraint.py
+++ MDRestraintsGenerator/tests/test_FindBoreschRestraint.py
@@ -5,7 +5,8 @@ Unit and regression test for the MDRestraintsGenerator package.
 import MDAnalysis as mda
 from MDRestraintsGenerator import search
 from MDRestraintsGenerator.restraints import FindBoreschRestraint
-from .datafiles import T4_TPR, T4_XTC, T4_OGRO, T4_OTOP
+from .datafiles import (T4_TPR, T4_XTC, T4_OGRO, T4_OTOP,
+                        CB8_TPR, CB8_XTC, CB8_OGRO, CB8_OTOP,)
 from numpy.testing import assert_almost_equal, assert_equal
 import filecmp
 import pytest
@@ -16,6 +17,11 @@ import sys
 @pytest.fixture(scope='module')
 def u():
     return mda.Universe(T4_TPR, T4_XTC)
+
+
+@pytest.fixture(scope='module')
+def u_guest():
+    return mda.Universe(CB8_TPR, CB8_XTC)
 
 
 def test_basic_regression_oldhostsearch(tmpdir, u):
@@ -30,7 +36,6 @@ def test_basic_regression_oldhostsearch(tmpdir, u):
 
     with tmpdir.as_cwd():
         find.run()
-
         find.restraint.write()
         dG = find.restraint.standard_state()
 
@@ -40,6 +45,30 @@ def test_basic_regression_oldhostsearch(tmpdir, u):
         assert_almost_equal(u_gro.atoms.positions, u_gro_ref.atoms.positions)
         assert filecmp.cmp(T4_OTOP, 'BoreschRestraint.top')
         assert_almost_equal(dG, -6.592, 2)
+
+
+def test_hostguest_regression_oldhostsearch(tmpdir, u_guest):
+    """Regression test for a Host/Guest system"""
+    l_atoms = [155, 157, 165]
+
+    p_atoms = search.find_host_atoms(u_guest, l_atoms[0],
+                                     p_selection="resname HST and not name H*")
+
+    atom_set = [(l_atoms, p) for p in p_atoms]
+
+    find = FindBoreschRestraint(u_guest, atom_set)
+
+    with tmpdir.as_cwd():
+        find.run()
+        find.restraint.write()
+        dG = find.restraint.standard_state()
+
+        u_gro = mda.Universe('ClosestRestraintFrame.gro')
+        u_gro_ref = mda.Universe(CB8_OGRO)
+
+        assert_almost_equal(u_gro.atoms.positions, u_gro_ref.atoms.positions)
+        assert filecmp.cmp(CB8_OTOP, 'BoreschRestraint.top')
+        assert_almost_equal(dG, -6.365, 2)
 
 
 def test_basic_regression(tmpdir, u):
@@ -55,7 +84,6 @@ def test_basic_regression(tmpdir, u):
 
     with tmpdir.as_cwd():
         find.run()
-
         find.restraint.write()
         dG = find.restraint.standard_state()
 
@@ -65,6 +93,31 @@ def test_basic_regression(tmpdir, u):
         assert_almost_equal(u_gro.atoms.positions, u_gro_ref.atoms.positions)
         assert filecmp.cmp(T4_OTOP, 'BoreschRestraint.top')
         assert_almost_equal(dG, -6.592, 2)
+
+
+def test_hostguest_regression(tmpdir, u_guest):
+    """Regression test for a Host/Guest system"""
+    l_atoms = [155, 157, 165]
+
+    psearch = search.FindHostAtoms(u_guest, l_atoms[0],
+                                   p_selection="resname HST and not name H*")
+    psearch.run(stop=1)
+
+    atom_set = [(l_atoms, p) for p in psearch.host_atoms]
+
+    find = FindBoreschRestraint(u_guest, atom_set)
+
+    with tmpdir.as_cwd():
+        find.run()
+        find.restraint.write()
+        dG = find.restraint.standard_state()
+
+        u_gro = mda.Universe('ClosestRestraintFrame.gro')
+        u_gro_ref = mda.Universe(CB8_OGRO)
+
+        assert_almost_equal(u_gro.atoms.positions, u_gro_ref.atoms.positions)
+        assert filecmp.cmp(CB8_OTOP, 'BoreschRestraint.top')
+        assert_almost_equal(dG, -6.365, 2)
 
 
 def test_basic_regression_ligand_search(u):
