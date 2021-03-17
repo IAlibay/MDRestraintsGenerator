0378e544c6babcdbdf1f1c3a63c75433ef497bd1
IAlibay <irfan.alibay@gmail.com>
2021-03-17 23:07:21 +0000

    datatypes tests

--- MDRestraintsGenerator/tests/test_datatypes.py
+++ MDRestraintsGenerator/tests/test_datatypes.py
@@ -63,6 +63,145 @@ def test_cog_com_distance_multiframe_basic(u, class_name):
 
 
 @pytest.mark.parametrize('frames', [None, 10])
+def test_FlatBottomRestraint_frame(u, frames):
+    lig = u.select_atoms('resname LIG')
+    prot = u.select_atoms('protein')
+
+    if frames is None:
+        frames = u.trajectory.n_frames
+        obj = dtypes.FlatBottomRestraint(lig, prot)
+    else:
+        obj = dtypes.FlatBottomRestraint(lig, prot, n_frames=frames)
+
+    assert obj.n_frames == frames
+    assert obj.com.values.size == frames
+
+
+@pytest.mark.parametrize('frame', [None, 10])
+def test_FlatBottom_plotting(tmpdir, u, frame):
+    lig = u.select_atoms('resname LIG')
+    prot = u.select_atoms('protein')    
+
+    obj = dtypes.FlatBottomRestraint(lig, prot)
+    obj.analyze()
+
+    with tmpdir.as_cwd():
+        obj.plot(frame=frame)
+        assert os.path.isfile('COM-distance_1.png')
+
+
+def test_FlatBottom_plotting_path(tmpdir, u):
+    lig = u.select_atoms('resname LIG')
+    prot = u.select_atoms('protein')    
+
+    obj = dtypes.FlatBottomRestraint(lig, prot)
+    obj.analyze()
+
+    with tmpdir.as_cwd():
+        obj.plot(frame=None, path='testdir')
+        assert os.path.isfile('./testdir/COM-distance_1.png')
+
+
+def test_FlatBottom_plotting_notanalysed(tmpdir, u):
+    lig = u.select_atoms('resname LIG')
+    prot = u.select_atoms('protein')    
+
+    obj = dtypes.FlatBottomRestraint(lig, prot)
+
+    with pytest.raises(AttributeError, match="not been analyzed yet"):
+        obj.plot()
+
+
+def test_FlatBottom_write_path(tmpdir, u_gro):
+    lig = u_gro.select_atoms('resname LIG')
+    prot = u_gro.select_atoms('protein')
+
+    obj = dtypes.FlatBottomRestraint(lig, prot)
+    obj.store_frame(0)
+    obj.analyze()
+
+    with tmpdir.as_cwd():
+        Path('./testdir').mkdir()
+        obj.write(frame=0, path='./testdir')
+
+        u2_gro = mda.Universe('./testdir/ClosestRestraintFrame.gro')
+        assert_almost_equal(u_gro.atoms.positions, u2_gro.atoms.positions)
+        os.path.isfile('./testdir/flatbottom_index.ndx')
+
+
+def test_FlatBottom_write_noframe(u_gro):
+    lig = u_gro.select_atoms('resname LIG')
+    prot = u_gro.select_atoms('protein')
+
+    obj = dtypes.FlatBottomRestraint(lig, prot)
+
+    with pytest.raises(RuntimeError, match="no frame defined for writing"):
+        obj.write()
+
+
+def test_FlatBottom_write_no_walldistance(u_gro):
+    lig = u_gro.select_atoms('resname LIG')
+    prot = u_gro.select_atoms('protein')
+
+    obj = dtypes.FlatBottomRestraint(lig, prot)
+    obj.store_frame(0)
+
+    with pytest.raises(RuntimeError, match="The `wall_distance` attribute"):
+        obj.write(frame=0)
+
+
+def test_FlatBottom_write_noimplemented(u_gro):
+    lig = u_gro.select_atoms('resname LIG')
+    prot = u_gro.select_atoms('protein')
+
+    obj = dtypes.FlatBottomRestraint(lig, prot)
+    obj.store_frame(0)
+    obj.analyze()
+
+    with pytest.raises(RuntimeError, match="not implemented yet"):
+        obj.write(outtype="AMBER")
+
+
+def test_nearest_com_atom(u_gro):
+    ag = u_gro.select_atoms('resid 0')
+
+    ix = dtypes.FlatBottomRestraint._get_nearest_com_atom(ag)
+
+    assert ix == 9
+
+
+def test_standard_state(u_gro):
+    lig = u_gro.select_atoms('resname LIG')
+    prot = u_gro.select_atoms('protein')
+
+    obj = dtypes.FlatBottomRestraint(lig, prot)
+    obj.store_frame(0)
+
+    dG = obj.standard_state(wall_distance=3.3)
+    assert_almost_equal(dG, -1.42, decimal=2)
+
+
+def test_standard_state_no_walldistance_error(u_gro):
+    lig = u_gro.select_atoms('resname LIG')
+    prot = u_gro.select_atoms('protein')
+
+    obj = dtypes.FlatBottomRestraint(lig, prot)
+
+    with pytest.raises(AttributeError, match="`wall_distance` was not"):
+        obj.standard_state()
+
+
+def test_standard_state_not_implemented_error(u_gro):
+    lig = u_gro.select_atoms('resname LIG')
+    prot = u_gro.select_atoms('protein')
+
+    obj = dtypes.FlatBottomRestraint(lig, prot)
+
+    with pytest.raises(NotImplementedError, match="not implemented"):
+        obj.standard_state(wall_distance=3.3, calc_type="numerical")
+
+
+@pytest.mark.parametrize('frames', [None, 10])
 def test_BoreschRestraint_frame(u, frames):
     l_atoms = [0, 1, 2]
     p_atoms = [4, 5, 6]
